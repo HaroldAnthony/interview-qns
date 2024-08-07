695517a45a4fb19b5e874a99a44f411551088bc0
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-08-07 12:55:58 +0530

    program 1 done

--- Tech Mahindra/reverse.js
+++ Tech Mahindra/reverse.js
@@ -1,3 +1,6 @@
 let a = 123456;
-let b = a.toString().split('').reverse().join('');
-console.log(b);
+let b = a.toString(); //converts number into a string
+let c = b.split('');  // "split('')" converts a string into an array (because reverse is an array method!)
+let d = c.reverse();  // reverse() reverses the characters within the array
+let e = d.join('');   // "join('')" converts an array into a string
+console.log(e);
