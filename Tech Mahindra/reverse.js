7c7a46bbba777bb0f1a8461db6745de1e45f8526
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-08-07 13:22:12 +0530

    done wid dis

--- Tech Mahindra/reverse.js
+++ Tech Mahindra/reverse.js
@@ -1,9 +1,13 @@
+//BASIC PROGRAM
+
 // let a = 123456;
 // let b = a.toString(); //converts number into a string
 // let c = b.split('');  // "split('')" converts a string into an array (because reverse is an array method!)
 // let d = c.reverse();  // reverse() reverses the characters within the array
 // let e = d.join('');   // "join('')" converts an array into a string
 // console.log(e);
+
+// BUILT A FUNCTION, BECAUSE THE INTERVIEW QUESTION REQUIRED TO
 
 function reverse(number){
     let a = parseInt(number);
