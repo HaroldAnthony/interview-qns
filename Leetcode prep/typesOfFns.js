93a2571826b37d41543dd99da4a76195c2dd9420
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-09-25 13:05:20 +0530

    anon fn

--- Leetcode prep/typesOfFns.js
+++ Leetcode prep/typesOfFns.js
@@ -1,8 +1,18 @@
-//NORMAL FUNCTION
+//NORMAL FN
 
-function f(a,b){
+// function f(a,b){
+//     const sum = a+b;
+//     return sum;
+// };
+
+// console.log(f(2,3));
+
+//ANONYMOUS FN
+
+let f = function(a,b){
     const sum = a+b;
     return sum;
-};
+}
 
 console.log(f(2,3));
+
