646e5959367957af637244d904291a31e9df006d
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-09-25 13:10:22 +0530

    immediate invoke

--- Leetcode prep/typesOfFns.js
+++ Leetcode prep/typesOfFns.js
@@ -9,10 +9,19 @@
 
 //ANONYMOUS FN
 
+// let f = function(a,b){
+//     const sum = a+b;
+//     return sum;
+// }
+
+// console.log(f(2,3));
+
+//IMMEDIATE INVOKE
+
 let f = function(a,b){
     const sum = a+b;
     return sum;
-}
+}(2,3)
 
-console.log(f(2,3));
+console.log(f);
 
