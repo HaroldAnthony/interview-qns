022dbd2d2a2df22a7645de298ec239a60dccde74
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-09-25 13:54:02 +0530

    d 25-09-24

--- Leetcode prep/typesOfFns.js
+++ Leetcode prep/typesOfFns.js
@@ -36,14 +36,17 @@
 
 //function within a function
 
-function fun(){
-    function f(a,b){
-        const sum = a+b;
-        return sum;
-    }
-    return f;
-}
+// function fun(){
+//     function f(a,b){
+//         const sum = a+b;
+//         return sum;
+//     }
+//     return f;
+// }
 
-var numsum = fun();
-console.log(numsum(2,3));
+// var numsum = fun();
+// console.log(numsum(2,3));
+
+
+
 
