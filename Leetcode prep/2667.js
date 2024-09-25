a41e8db787c6f4f4c5e0beb3dcd8878c330ad678
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-09-25 12:56:58 +0530

    2667 e

--- Leetcode prep/2667.js
+++ Leetcode prep/2667.js
@@ -2,3 +2,10 @@
 
 //Write a function createHelloWorld. It should return a new function that always returns "Hello World".
 
+function createHelloWorld(){
+    return function inner(){
+        return "Hello World";
+    };
+};
+
+createHelloWorld();
